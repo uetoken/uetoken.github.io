217b962269570a2529f67b1be7faa3556fa176a5
Useless Ethereum Token CEO <uetokenceo@gmail.com>
2017-07-03 19:39:09 -0700

    Add stats tracker

--- uetoken.js
+++ uetoken.js
@@ -1,1 +1,90 @@
 console.log("nothing to see here, move along");
+
+function decodeStats(response, price) {
+    if (response == null) return null;
+
+    var result = response.result;
+    if (result == null || result.length == null || result.length < 193) return null;
+
+    var weiPerEther = new BigNumber("1000000000000000000", 10);
+
+    var totalContributionExact = new BigNumber(result.substr(0, 64), 16).div(weiPerEther);
+    var totalContributionUSDExact = totalContributionExact.times(new BigNumber(price));
+
+    return {
+        totalContribution: totalContributionExact.round(3, BigNumber.ROUND_DOWN),
+        totalContributionUSD: totalContributionUSDExact.round(0, BigNumber.ROUND_DOWN),
+        totalContributionTVs: totalContributionUSDExact.div(new BigNumber("1200")).round(0, BigNumber.ROUND_DOWN),
+        totalSupply: new BigNumber(result.substr(64, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
+        totalBonusTokensIssued: new BigNumber(result.substr(128, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
+        purchasingAllowed: new BigNumber(result.substr(192, 64), 16).isZero() == false
+    };
+}
+
+function getStats(price) {
+    var url = "https://api.etherscan.io/api?module=proxy&action=eth_call&to=0x27f706edde3aD952EF647Dd67E24e38CD0803DD6&data=0xc59d48470000000000000000000000000000000000000000000000000000000000000000&tag=latest";
+    return $.ajax(url, {
+        cache: false,
+        dataType: "json"
+    }).then(function (data) { return decodeStats(data, price); });
+}
+
+function getPrice() {
+    var url = "https://api.etherscan.io/api?module=stats&action=ethprice";
+    return $.ajax(url, {
+        cache: false,
+        dataType: "json"
+    }).then(function (data) {
+        if (data == null) return null;
+        if (data.result == null) return null;
+        if (data.result.ethusd == null) return null;
+
+        return parseFloat(data.result.ethusd);
+    });
+}
+
+function updatePage(stats) {
+    if (stats == null) return;
+    if (!stats.purchasingAllowed/* && Date.now < Date.UTC(2017, 6, 4, 14)*/) {
+        $("#stats").hide();
+        return;
+    }
+
+    $("#total-ether").text(stats.totalContribution.toFixed(3));
+    if (stats.totalContribution.toNumber() <= 0) {
+        $("#total-ether-message").text("Looks like everyone read the warnings so far.");
+    } else {
+        $("#total-ether-message").text("I had a feeling someone would waste their money.");
+    }
+
+    $("#total-usd").text("$" + stats.totalContributionUSD.toFixed(0));
+    if (stats.totalContributionTVs.toNumber() <= 0) {
+        $("#total-usd-message").text("No Ether yet, so no cash either.");
+    } else if (stats.totalContributionTVs.toNumber() < 1) {
+        $("#total-usd-message").text("Not enough to buy a television yet.");
+    }else if (stats.totalContributionTVs.toNumber() < 2) {
+        $("#total-usd-message").text("Enough to buy a television.");
+    } else {
+        $("#total-usd-message").text("Enough to buy " + stats.totalContributionTVs.toFixed(0) + " televisions!");
+    }
+
+    $("#total-tokens").text(stats.totalSupply.toFixed(3));
+    if (stats.totalSupply <= 0) {
+        $("#total-tokens-message").text("No useless tokens issued yet either.");
+    } else if (stats.totalBonusTokensIssued.toNumber() <= 0) {
+        $("#total-tokens-message").text("Look at all those useless tokens!");
+    } else {
+        $("#total-tokens-message").text("Including " + stats.totalBonusTokensIssued.toFixed(3) + " bonus tokens!");
+    }
+
+    $("#stats").show();
+}
+
+function refresh() { getPrice().then(getStats).then(updatePage); }
+
+$(function() {
+    try {
+        refresh();
+        setInterval(refresh, 1000 * 60 * 5);
+    } catch (err) { }
+});
